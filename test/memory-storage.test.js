7d6881470bdba745fe86d48ddb9266edde7cac93
HeroicYang <me@heroicyang.com>
2014-04-22 16:32:38 +0800

    add storage interface and memory storage

---
+++ test/memory-storage.test.js
@@ -0,0 +1,36 @@
+/**
+ * Test dependencies
+ */
+var should = require('should');
+var MemoryStorage = require('../lib/storages/memory');
+
+describe('MemoryStorage Constructor', function() {
+  var memoryStorage = new MemoryStorage();
+
+  it('should set correct name', function() {
+    should.exist(memoryStorage);
+    memoryStorage.name.should.eql('memory');
+  });
+
+  it('list(callback)', function(done) {
+    memoryStorage.list(function(err, data) {
+      should.not.exist(err);
+      should.exist(data);
+      done();
+    });
+  });
+
+  it('save(data, callback)', function(done) {
+    var data = {
+      Permission: [{ action: 'create', resource: 'post' }],
+      Role: [{ name: 'admin' }]
+    };
+    memoryStorage.save(data, function(err) {
+      should.not.exist(err);
+      memoryStorage.list(function(err, result) {
+        result.should.eql(data);
+        done();
+      });
+    });
+  });
+});
